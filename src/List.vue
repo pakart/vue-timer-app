82fb3c2aaa60a337126b48359aa5af468ae37853
Pak Artem <obscurexdlol@gmail.com>
2020-09-25 17:35:37 +0300

    clean up + comments

--- src/List.vue
+++ src/List.vue
@@ -1,7 +1,10 @@
+<!-- Компонент-список для отображения секундомеров -->
 <template>
   <div id="inner-app">
     <div class="timers-container">
+      <!-- Рендер необходимого количества компонентов-секундомеров -->
       <Timer v-for="timer in timerCounter" :key="timer"/>
+      <!-- Кнопка добавления нового компонента-секундомера -->
       <div class="add-timer-block" v-on:click="addTimer()">
         <div class="plus-icon"></div>
       </div>
@@ -14,12 +17,14 @@ import Timer from '@/components/Timer.vue';
 
 export default {
   data: () => ({
+    // Счётчик количества секундомеров
     timerCounter: 0,
   }),
   components: {
     Timer,
   },
   methods: {
+    // Метод обновления счётчика
     addTimer() {
       this.timerCounter += 1;
     },
@@ -28,61 +33,62 @@ export default {
 </script>
 
 <style>
+  /* Стили для корректного отображения адаптива внутреннего контейнера */
+  #inner-app {
+      font-family: Gotham Pro, Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      margin-top: 100px;
+      align-items: center;
+      flex-direction: row;
+      background-color: #353638;
+      width: 300px;
+  }
 
-#inner-app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+  @media (min-width: 768px) and (max-width: 1023px) {
+    #inner-app {
+      width: 600px;
+    }
+  }
+  @media  (min-width:1024px) {
+    #inner-app {
+      width: 900px;
+    }
+  }
+  /* Контейнер секундомеров */
+  .timers-container{
+    height: auto;
+    width: 100%;
     display: flex;
-    justify-content: center;
-    margin-top: 100px;
-    align-items: center;
     flex-direction: row;
-    background-color: #353638;
-    width: 300px;
-}
-
-@media (min-width: 768px) and (max-width: 1023px) {
-  #inner-app {
-    width: 600px;
+    flex-wrap: wrap;
+    padding-left: 50px;
   }
-}
-@media  (min-width:1024px) {
-  #inner-app {
-    width: 900px;
+  /* Кнопка добавления таймера */
+  .add-timer-block{
+    width: 225px;
+    height: 120px;
+    background-color: #696969;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-right: 50px;
+    margin-bottom: 25px;
   }
-}
-
-.timers-container{
-  height: auto;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding-left: 50px;
-}
-
-.add-timer-block{
-  width: 225px;
-  height: 120px;
-  background-color: #696969;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  margin-right: 50px;
-  margin-bottom: 25px;
-}
-.plus-icon{
-  display:inline-block;
-  width:40px;
-  height:40px;
-  background:
-    linear-gradient(#9E9E9E,#9E9E9E),
-    linear-gradient(#9E9E9E,#9E9E9E);
-  background-position:center;
-  background-size: 50% 3px,3px 50%;
-  background-repeat:no-repeat;
-}
+  /* css иконка плюса */
+  .plus-icon{
+    display:inline-block;
+    width:40px;
+    height:40px;
+    background:
+      linear-gradient(#9E9E9E,#9E9E9E),
+      linear-gradient(#9E9E9E,#9E9E9E);
+    background-position:center;
+    background-size: 50% 3px,3px 50%;
+    background-repeat:no-repeat;
+  }
 </style>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,10 +1,17 @@
+<!-- Компонент-секундомер с отображением времени, кнопками старт/пауза и сброса секундомера
+с динамической сменой стилей -->
 <template>
   <div class="timer-body">
-    <h3 class="string inactive" v-bind:class="{active: isActive}">{{resultTimeString}}</h3>
+    <!-- Элемент, отображаеющий текущее время секундомера -->
+    <div class="string inactive" v-bind:class="{active: isActive}">{{resultTimeString}}</div>
+    <!-- Элемент - горизонтальная линия -->
     <hr class="line inactive" v-bind:class="{active: isActive}">
+    <!-- Элемент-контейнер для кнопок -->
     <div class="buttons-container">
+      <!-- Кнопка старт-пауза -->
       <div class="button play"
       v-bind:class="{pause: isActive}" v-on:click="startStopTimer()"></div>
+      <!-- Кнопка сброса -->
       <div class="drop-button inactive"
       v-bind:class="{active: isActive}" v-on:click="dropTimer()"></div>
     </div>
@@ -14,15 +21,20 @@
 <script>
 export default {
   data: () => ({
+    // флаг состояния активности
     isActive: false,
+    // id таймера для управления
     timerId: 0,
+    // количество секунд
     seconds: 0,
+    // результирующая строка прошедшего времени
     resultTimeString: '0',
   }),
   methods: {
+    // метод запуска/остановки секундомера
     startStopTimer() {
+      // проверка состояния с последующим стартом/остановкой секундомера
       if (this.isActive === false) {
-        console.log('start');
         this.isActive = true;
         // eslint-disable-next-line prefer-arrow-callback
         this.timerId = setInterval(function () {
@@ -30,49 +42,62 @@ export default {
           this.resultTimeString = this.timeFormater(this.seconds);
         }.bind(this), 1000);
       } else {
-        console.log('stop');
         clearTimeout(this.timerId);
         this.isActive = false;
       }
     },
+    // метод конвертирования прошедших секунд в необходимый кастомный формат отображения времени
     timeFormater(sec) {
+      // прошло меньше минуты
       if (sec < 60) {
         return sec.toString();
       }
+      // прошло меньше часа
       if (sec < 3600) {
-        return `${Math.trunc(sec / 60).toString()}:${(sec % 60).toString()}`;
+        return `${Math.trunc(sec / 60).toString()}:${((sec % 60) < 10 ? `0${(sec % 60).toString()}` : (sec % 60).toString())}`;
       }
-      return `${Math.trunc(sec / 3600).toString()}:${Math.trunc((sec % 3600) / 60).toString()}:${((sec % 3600) % 60).toString()}`;
+      // час+
+      const mm = Math.trunc((sec % 3600) / 60).toString();
+      const ss = ((sec % 3600) % 60).toString();
+      return `${Math.trunc(sec / 3600).toString()}:${mm < 10 ? `0${mm}` : mm}:${ss < 10 ? `0${ss}` : ss}`;
     },
+    // метод сброса секундомера
     dropTimer() {
       clearTimeout(this.timerId);
       this.seconds = 0;
       this.resultTimeString = '0';
       this.isActive = false;
-      console.log('CLEARED');
     },
   },
 };
 </script>
 
 <style>
-
+  /* неактивное состояние строки времени */
   .string.inactive{
+    font-size: 22px;
+    font-weight: 400;
+    margin-top: 18px;
+    margin-bottom: 16px;
     color: #9E9E9E;
   }
+  /* активное состояние строки времени */
   .string.active{
     color: #FFFFFF;
   }
+   /* неактивное состояние полосы */
   .line.inactive{
     height: 1px;
     color: #9E9E9E;
     border-width: 0;
     background-color: #9E9E9E;
   }
+  /* активное состояние полосы */
   .line.active{
     color: #FFFFFF;
     background-color: #FFFFFF;
   }
+  /* стили контейнера кнопок */
   .buttons-container{
     margin-top: 20px;
     display: flex;
@@ -80,6 +105,7 @@ export default {
     align-items: center;
     justify-content: center;
   }
+  /* контейнер секундомера */
   .timer-body {
     width: 225px;
     height: 120px;
@@ -88,6 +114,7 @@ export default {
     margin-bottom: 45px;
   }
 
+  /* css иконка "play" */
   .button.play {
     margin-right: 50px;
     width: 20px;
@@ -97,6 +124,7 @@ export default {
     border-width: 10px 0px 10px 20px;
     border-color: transparent transparent transparent #9E9E9E;
   }
+  /* css иконка "pause" */
   .button.pause {
     width: 20px;
     height: 20px;
@@ -104,12 +132,14 @@ export default {
     border-width: 0px 0px 0px 10px;
     border-color: #FFFFFF;
   }
+  /* неактивная css иконка "reset" */
   .drop-button.inactive {
     width: 20px;
     border-style: solid;
     border-width: 20px 0px 0px 0px;
     border-color:#9E9E9E;
   }
+  /* активная css иконка "reset" */
   .drop-button.active{
     border-color:#FFFFFF;
   }
